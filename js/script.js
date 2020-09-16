5651f8971850b3e170f7eb4c2b62791055b6bf5f
Aleksey <alexey-step@mail.ru>
2020-09-16 16:34:28 +0500

    Выполнил задание Учебный проект:начинаем программировать

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getFireballSpeed = function (isMovingLeft) {
+  return isMovingLeft ? 2 : 5;
+};
+
+var getWizardHeight = function() {
+  return 1.337 * wizardWidth;
+};
+
+var getWizardX = function (gameFieldWidth) {
+  return (gameFieldWidth - wizardWidth) / 2;
+};
+
+var getWizardY = function (gameFieldHeight) {
+  return gameFieldHeight / 3;
+};
