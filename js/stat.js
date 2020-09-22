7578c745b3817431c5f17648880c8250d7edbb8b
Aleksey <alexey-step@mail.ru>
2020-09-22 17:15:53 +0500

    Правки по улучшению кода

--- js/stat.js
+++ js/stat.js
@@ -18,9 +18,9 @@ const renderCloud = (ctx, x, y, color) => {
 };
 
 const renderTitle = (ctx, x, y, text) => {
-  ctx.fillStyle = "#000";
-  ctx.font = "16px PT Mono";
-  ctx.textBaseline = "hanging";
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
   ctx.fillText(
       text,
       x,
@@ -40,7 +40,7 @@ const getMaxElement = (arr) => {
 };
 
 const getRandomColor = () => {
-  return "hsl(240," + Math.floor(Math.random() * 100) + "%, 50%)";
+  return `hsl(240 ${Math.floor(Math.random() * 100)}% 50%)`;
 };
 
 const renderBar = (ctx, x, y, width, height) => {
@@ -58,28 +58,25 @@ const renderTime = (ctx, time, x, y) => {
 
 window.renderStatistics = (ctx, names, times) => {
 
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.7)");
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
 
-  renderTitle(ctx, TITLE_X, TITLE_Y, "Ура вы победили!");
-  renderTitle(ctx, TITLE_X, TITLE_Y + TITLE_GAP, "Список результатов:");
-
-
-  let maxTime = getMaxElement(times);
+  renderTitle(ctx, TITLE_X, TITLE_Y, `Ура вы победили!`);
+  renderTitle(ctx, TITLE_X, TITLE_Y + TITLE_GAP, `Список результатов:`);
 
   for (let i = 0; i < names.length; i++) {
 
-    ctx.fillStyle = names[i] === "Вы" ? "hsl(0, 100%, 50%)" : getRandomColor();
+    const MAX_TIME = getMaxElement(times);
+    const BAR_HEIGHT = (BAR_MAXHEIGHT * times[i]) / MAX_TIME;
+    const BAR_X = CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_GAP) * i;
+    const USER_TIME = Math.floor(times[i]);
 
-    let barHeight = (BAR_MAXHEIGHT * times[i]) / maxTime;
-    let barX = CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_GAP) * i;
+    ctx.fillStyle = names[i] === `Вы` ? `hsl(0, 100%, 50%)` : getRandomColor();
 
-    renderBar(ctx, barX, CLOUD_HEIGHT - GAP * 3, BAR_WIDTH, -barHeight);
+    renderBar(ctx, BAR_X, CLOUD_HEIGHT - GAP * 3, BAR_WIDTH, -BAR_HEIGHT);
 
-    renderName(ctx, names[i], barX, CLOUD_HEIGHT - GAP * 2, "#000");
+    renderName(ctx, names[i], BAR_X, CLOUD_HEIGHT - GAP * 2, `#000`);
 
-    let userTime = Math.floor(times[i]);
-
-    renderTime(ctx, userTime, barX, CLOUD_HEIGHT - GAP * 5 - barHeight);
+    renderTime(ctx, USER_TIME, BAR_X, CLOUD_HEIGHT - GAP * 5 - BAR_HEIGHT);
   }
 };
